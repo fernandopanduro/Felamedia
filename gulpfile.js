86c117810501303c2a73e6559c0321bc3c30072f
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-02-26 18:13:02 -0700

    Gulpfile, minificar images y transformarlas en formato avif y webp

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const { src, dest, parallel } = require('gulp')
+
+// Imagnes
+const cache    = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp     = require('gulp-webp');
+const avif     = require('gulp-avif')
+
+function imageMin(done) {
+
+    const options = {
+        optimizationLevel: 3,
+    };
+
+    src('./Assets/Img/*{png,jpg}')      //Buscar 
+        .pipe(cache(imagemin(options))) //Minificar
+        .pipe(dest('./Assets/Img'))     //Guardar
+
+    done();
+};
+
+function versionWebp(done) {
+    
+    const options = {
+        quality: 50,
+    };
+
+    src('./Assets/Img/*{png,jpg}') //Buscar 
+        .pipe(webp(options))       //Webp
+        .pipe(dest('./Assets/Img'))//Guardar
+
+    done()
+};
+
+function versionAvif(done) {
+    
+    const options = {
+        quality: 50,
+    };
+
+    src('./Assets/Img/*{png,jpg}')  //Buscar
+        .pipe(avif(options))        //Avif
+        .pipe(dest('./Assets/Img')) //Guardar
+
+    done()
+}
+
+
+exports.imageMin    = imageMin;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev         = parallel(versionAvif, versionWebp);
