2925333a0d42b3456282674d80d6db3e8f047cba
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-02-28 10:07:45 -0700

    Form

--- JS/Script.js
+++ JS/Script.js
@@ -17,3 +17,53 @@ function animateBars() {
     lineBar3.classList.toggle('bars__lineBar3--active');
     responsiveMenu.classList.toggle('responsiveMenu--active')
 }
+
+
+/* Form */
+
+/* const form      = document.querySelector('.form');
+const fullName  = document.querySelector('#full-name');
+const email     = document.querySelector('#email');
+const phone     = document.querySelector('#phone');
+const subject   = document.querySelector('#subject');
+const message   = document.querySelector('#message');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+})
+
+const setError = (element, menssage) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error')
+
+    errorDisplay.innerText = menssage;
+    inputControl.classList.add('error')
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = (element, menssage) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error')
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success')
+    inputControl.classList.remove('error')
+}
+        
+const validateInputs = () => {
+    const nameValue = fullName.value.trim()
+    const emailValue = email.value.trim()
+    const phoneValue = phone.value.trim()
+    const subjectValue = subject.value.trim()
+    const messageValue = message.value.trim()
+
+    if (nameValue === '') {
+        setError(nameValue, 'Name is required')
+    } else {
+        setSuccess(nameValue)
+    }
+}
+
+console.log(email.value) */
